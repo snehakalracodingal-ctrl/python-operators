fb555c8a1201a0f1a4264ef919bc7045c0bd13bf
snehakalracodingal-ctrl <snehakalra.codingal@gmail.com>
2026-01-07 16:14:19 +0530

    qaciju

---
+++ not_equal.py
@@ -0,0 +1,16 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print(a, 'and', b, 'are different.')
+
+else:
+    print(a, 'and', b, 'are same.') 
